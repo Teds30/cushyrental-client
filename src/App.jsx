e70d77fc181abd3f1d4bd0244ae4c83c663b3914
JohnOtilla09 <otillajohn09@gmail.com>
2023-08-16 19:59:41 +0800

    DP-26 | initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-import { useState } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import CreateAccount from "./pages/auth/CreateAccount/CreateAccount";
 // import './App.css'
 
 function App() {
-    return (
-        <Routes>
-            <Route
-                path="/login"
-                element={
-                    <>
-                        <h1>Login</h1>
-                    </>
-                }
-            ></Route>
-            <Route
-                path="/register"
-                element={
-                    <>
-                        <h1>Register</h1>
-                    </>
-                }
-            ></Route>
-            <Route path="*" element={<Navigate replace to="/login" />}></Route>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route
+        path="/login"
+        element={
+          <>
+            <h1>Login</h1>
+          </>
+        }
+      ></Route>
+      <Route path="/register" element={<CreateAccount />}></Route>
+      <Route path="*" element={<Navigate replace to="/register" />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/auth/CreateAccount/CreateAccount.jsx
@@ -0,0 +1,9 @@
+const CreateAccount = () => {
+    return (
+        <div>
+            <h1>Create Account</h1>
+        </div>
+    );
+}
+
+export default CreateAccount;
