6c2e4b014700bcbc20c86fc470719a6050226df8
Arjie2002 <johnarjiedalisay.beredo@bicol-u.edu.ph>
2023-08-18 13:10:30 +0800

    DP-27 | Login Account

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import CreateAccount from "./pages/auth/CreateAccount/CreateAccount";
+import SignInPage from "./pages/Login/SigninPage";
+import ForgotPassword from "./pages/Login/ForgotPassword";
 // import './App.css'
 
 function App() {
@@ -14,8 +16,14 @@ function App() {
           </>
         }
       ></Route>
+
+      <Route path="/signinpage" element={<SignInPage />}></Route>
+      <Route path="/signinpage/forgotpassword" element={<ForgotPassword />}></Route>
+
       <Route path="/register" element={<CreateAccount />}></Route>
       <Route path="*" element={<Navigate replace to="/register" />}></Route>
+
+
     </Routes>
   );
 }
