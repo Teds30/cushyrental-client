c2fd71f4ded4a0975549083b52409dfe61224d76
JohnOtilla09 <otillajohn09@gmail.com>
2023-08-20 12:55:33 +0800

    google-auth-hook | create google-hook

---
+++ src/hooks/data/google-auth-hook.js
@@ -0,0 +1,35 @@
+import { useCallback, useContext, useState } from 'react'
+import useHttp from '../http-hook'
+
+const googleAuth = () => {
+    const { sendRequest, isLoading } = useHttp()
+
+    const googleAccountRegistration = useCallback(
+        async (body) => {
+            let responseData;
+            try {
+                    responseData = await sendRequest({
+                    url: `http://127.0.0.1:8000/api//auth/google/redirect`,
+                    method: 'POST',
+                    body: JSON.stringify(body),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+
+                console.log(responseData);
+                return responseData;
+            } catch (err) {
+                throw err.message
+            }
+        },
+        [sendRequest]
+    )
+
+    return {
+        isLoading,
+        googleAccountRegistration
+    }
+}
+
+export default googleAuth
