7b199c9919f108498353dc6f64d8330a58b19429
JohnOtilla09 <johncanila.otilla@bicol.-u.edu.ph>
2023-08-27 23:45:23 +0800

    main | add NotifyProvider and AuthContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import NotifyProvider from "./components/Feedback/NotifyProvider.jsx";
+import { AuthContextProvider } from "./context/auth-context.jsx";
 
-import App from './App.jsx'
-import './index.css'
-import './GlobalCss.css'
+import App from "./App.jsx";
+import "./index.css";
+import "./GlobalCss.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-        <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <BrowserRouter>
+      <NotifyProvider>
         <GoogleOAuthProvider clientId="289444714327-s692ob9granvnk1pr1lk0csn44fpptqu.apps.googleusercontent.com">
+          <AuthContextProvider>
             <App />
-            </GoogleOAuthProvider>
-        </BrowserRouter>
-    </>
-)
+          </AuthContextProvider>
+        </GoogleOAuthProvider>
+      </NotifyProvider>
+    </BrowserRouter>
+  </>
+);
