cb7d3c1960d5ddb5a9b1207108512bbbcd5d12de
JohnOtilla09 <johncanila.otilla@bicol.-u.edu.ph>
2023-08-27 23:47:23 +0800

    auth-context | add auth context

---
+++ src/context/auth-context.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+const AuthContext = React.createContext({
+    user: {},
+    token: '',
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (user, token) => {}
+});
+
+export const AuthContextProvider = (props) => {
+    const [ user, setUser ] = useState({});
+    const [ token, setToken ] = useState('');
+    const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+        setToken('');
+        setUser({});
+    };
+
+    const loginHandler = (user, token) => {
+        setToken(token);
+        setUser(user);
+        setIsLoggedIn(true);
+    };
+
+    return (
+        <AuthContextProvider value={{ user: user, token: token, isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler }}>
+            {props.children}
+        </AuthContextProvider>
+    );
+}
+
+export default AuthContext;
