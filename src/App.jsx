08ac911473e5ba0899ace042ce4f848d59ae3923
Ted30 <teddymarcenaje@gmail.com>
2023-08-29 22:33:28 +0800

    added chat system routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import CreateAccount from "./pages/auth/CreateAccount/CreateAccount";
-import SignInPage from "./pages/Login/SigninPage";
-import ForgotPassword from "./pages/Login/ForgotPassword";
-import Conversation from "./pages/chat/Conversation";
+import { useState } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import CreateAccount from './pages/auth/CreateAccount/CreateAccount'
+import SignInPage from './pages/Login/SigninPage'
+import ForgotPassword from './pages/Login/ForgotPassword'
+import Conversation from './pages/chat/Conversation'
+import Chats from './pages/chat/Chats'
 // import './App.css'
 
+import { AuthContext } from './context/AuthContext'
+import { useAuth } from './hooks/auth-hook'
+
 function App() {
-  return (
-    <Routes>
-      <Route
-        path="/login"
-        element={
-          <>
-            <h1>Login</h1>
-          </>
-        }
-      ></Route>
-      
-      <Route path="/chats/:room_id" element={<Conversation />}></Route>
+    const { userId, login } = useAuth()
+    return (
+        <AuthContext.Provider
+            value={{
+                userId: userId,
+                login: login,
+            }}
+        >
+            <Routes>
+                <Route
+                    path="/login"
+                    element={
+                        <>
+                            <h1>Login</h1>
+                        </>
+                    }
+                ></Route>
 
-      <Route path="/signinpage" element={<SignInPage />}></Route>
-      <Route path="/signinpage/forgotpassword" element={<ForgotPassword />}></Route>
+                <Route path="/chats/" element={<Chats />}></Route>
+                <Route
+                    path="/chats/:room_id"
+                    element={<Conversation />}
+                ></Route>
 
-      <Route path="/register" element={<CreateAccount />}></Route>
+                <Route path="/signinpage" element={<SignInPage />}></Route>
+                <Route
+                    path="/signinpage/forgotpassword"
+                    element={<ForgotPassword />}
+                ></Route>
 
-      <Route path="*" element={<Navigate replace to="/signinpage" />}></Route>
+                <Route path="/register" element={<CreateAccount />}></Route>
 
-
-    </Routes>
-  );
+                <Route
+                    path="*"
+                    element={<Navigate replace to="/signinpage" />}
+                ></Route>
+            </Routes>
+        </AuthContext.Provider>
+    )
 }
 
-export default App;
+export default App
