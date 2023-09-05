076a20fb184fdb13ee708cfbc96351105fe6fc70
JohnOtilla09 <johncanila.otilla@bicol.-u.edu.ph>
2023-09-06 00:24:37 +0800

    Quantity | edit quantity component

--- src/components/Quantity/Quantity.jsx
+++ src/components/Quantity/Quantity.jsx
@@ -1,43 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { TbPlus, TbMinus } from 'react-icons/tb'
+import { TbPlus, TbMinus } from "react-icons/tb";
 
-import styles from './Quantity.module.css'
+import styles from "./Quantity.module.css";
 
 const Quantity = (props) => {
-    const { maxValue = 0 } = props
+    const { maxValue = 0, setQuantityvalue = 0 } = props;
 
-    const [value, setValue] = useState(0)
+    const [value, setValue] = useState(setQuantityvalue);
 
     const incrementHandler = () => {
         if (value + 1 > maxValue) {
-            return
+            return;
         }
 
-        setValue(value + 1)
-        props.onQuantity({ value: value + 1 })
-    }
+        setValue(value + 1);
+        props.onQuantity({ value: value + 1 });
+    };
 
     const decrementHandler = () => {
         if (value === 0) {
-            return
+            return;
         }
 
-        setValue(value - 1)
-        props.onQuantity({ value: value - 1 })
-    }
+        setValue(value - 1);
+        props.onQuantity({ value: value - 1 });
+    };
 
     return (
         <div className={styles.card}>
-            <button className={styles.button} onClick={decrementHandler}>
+            <button type="button" className={styles.button} onClick={decrementHandler}>
                 <TbMinus size={24} />
             </button>
             <div className={styles.count}>{value}</div>
-            <button className={styles.button} onClick={incrementHandler}>
+            <button type="button" className={styles.button} onClick={incrementHandler}>
                 <TbPlus size={24} />
             </button>
         </div>
-    )
-}
+    );
+};
 
-export default Quantity
+export default Quantity;
