2841ffd4ae9e1dc8ea319d73270db3268463fac3
JohnOtilla09 <johncanila.otilla@bicol.-u.edu.ph>
2023-09-12 08:58:45 +0800

    unit-hook | update unit hook

--- src/hooks/data/units-hook.js
+++ src/hooks/data/units-hook.js
@@ -2,63 +2,151 @@ import { useCallback, useContext, useState } from "react";
 import useHttp from "../http-hook";
 
 const useUnitManager = () => {
-  const { sendRequest, isLoading } = useHttp();
+    const { sendRequest, isLoading } = useHttp();
 
-  const fetchUnits = useCallback(async () => {
-    let responseData;
-    try {
-      responseData = await sendRequest({
-        url: `http://127.0.0.1:8000/api/units`,
-      });
-    } catch (err) {
-      throw err.message;
-    }
+    const fetchUnits = useCallback(async () => {
+        let responseData;
+        try {
+            responseData = await sendRequest({
+                url: `http://127.0.0.1:8000/api/units`,
+            });
+        } catch (err) {
+            throw err.message;
+        }
 
-    return responseData;
-  }, [sendRequest]);
+        return responseData;
+    }, [sendRequest]);
 
-  const fetchUnit = useCallback(
-    async (id) => {
-      let responseData;
-      try {
-        responseData = await sendRequest({
-          url: `http://127.0.0.1:8000/api/units/${id}`,
-        });
-      } catch (err) {
-        throw err.message;
-      }
+    const fetchUnit = useCallback(
+        async (id) => {
+            let responseData;
+            try {
+                responseData = await sendRequest({
+                    url: `http://127.0.0.1:8000/api/units/${id}`,
+                });
+            } catch (err) {
+                throw err.message;
+            }
 
-      return responseData;
-    },
-    [sendRequest]
+            return responseData;
+        },
+        [sendRequest]
+    );
+
+    const updateUnit = useCallback(
+        async (id, body) => {
+            let responseData;
+            try {
+                responseData = await sendRequest({
+                    url: `http://127.0.0.1:8000/api/units/${id}`,
+                    method: "PUT",
+                    body: JSON.stringify(body),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+            } catch (err) {
+                throw err.message;
+            }
+
+            return responseData;
+        },
+        [sendRequest]
+    );
+
+    const fetchUnitAmenities = useCallback(
+        async (id, body) => {
+            let responseData;
+            try {
+                responseData = await sendRequest({
+                    url: `http://127.0.0.1:8000/api/unit_amenities/${id}`,
+                    method: "GET",
+                    body: JSON.stringify(body),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+            } catch (err) {
+                throw err.message;
+            }
+
+            return responseData;
+        },
+        [sendRequest]
+    );
+
+    const fetchUnitFacilities = useCallback(
+        async (id, body) => {
+            let responseData;
+            try {
+                responseData = await sendRequest({
+                    url: `http://127.0.0.1:8000/api/unit_facilities/${id}`,
+                    method: "GET",
+                    body: JSON.stringify(body),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+            } catch (err) {
+                throw err.message;
+            }
+
+            return responseData;
+        },
+        [sendRequest]
+    );
+
+    const fetchUnitInclusions = useCallback(
+      async (id, body) => {
+          let responseData;
+          try {
+              responseData = await sendRequest({
+                  url: `http://127.0.0.1:8000/api/unit_inclusions/${id}`,
+                  method: "GET",
+                  body: JSON.stringify(body),
+                  headers: {
+                      "Content-Type": "application/json",
+                  },
+              });
+          } catch (err) {
+              throw err.message;
+          }
+
+          return responseData;
+      },
+      [sendRequest]
   );
 
-  const updateUnit = useCallback(
+  const fetchUnitRules = useCallback(
     async (id, body) => {
-      let responseData;
-      try {
-        responseData = await sendRequest({
-          url: `http://127.0.0.1:8000/api/units/${id}`,
-          method: "PUT",
-          body: JSON.stringify(body),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-      } catch (err) {
-        throw err.message;
-      }
+        let responseData;
+        try {
+            responseData = await sendRequest({
+                url: `http://127.0.0.1:8000/api/unit_rules/${id}`,
+                method: "GET",
+                body: JSON.stringify(body),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        } catch (err) {
+            throw err.message;
+        }
 
-      return responseData;
+        return responseData;
     },
     [sendRequest]
-  );
+);
 
-  return {
-    isLoading,
-    fetchUnits,
-    fetchUnit,
-  };
+    return {
+        isLoading,
+        fetchUnits,
+        fetchUnit,
+        fetchUnitAmenities,
+        fetchUnitFacilities,
+        fetchUnitInclusions,
+        fetchUnitRules
+    };
 };
 
 export default useUnitManager;
