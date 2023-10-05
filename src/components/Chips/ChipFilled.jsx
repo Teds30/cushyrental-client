ff730777f60ec50c5b92bf90b3a3db60e6f333f0
JohnOtilla09 <johncanila.otilla@bicol.-u.edu.ph>
2023-10-05 13:49:36 +0800

    Chip | update chip component

--- src/components/Chips/ChipFilled.jsx
+++ src/components/Chips/ChipFilled.jsx
@@ -1,64 +1,112 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
+
+import useImageManager from "../../hooks/data/image-hook";
 
 import styles from "./ChipButton.module.css";
 
 const ChipFilled = (props) => {
-  const { items, button = "checkbox", clickable = true } = props;
+    const {
+        items: originalItems = [], // Rename originalItems to avoid conflicts
+        button = "checkbox",
+        clickable = true,
+        selected = [],
+    } = props;
 
-  const [chips, setChips] = useState([]);
+    const { fetchIcon } = useImageManager();
 
-  const handleClick = (id) => {
-    if (button === "checkbox") {
-      if (chips.includes(id)) {
-        setChips(chips.filter((chipId) => chipId !== id));
-        props.onChipValue(chips.filter((chipId) => chipId !== id));
-      } else {
-        setChips([...chips, id]);
-        props.onChipValue([...chips, id]);
-      }
-    } else {
-      setChips(id);
-      props.onChipValue(id);
-    }
-  };
+    const [chips, setChips] = useState(selected.length !== 0 ? selected : []);
+    const [items, setItems] = useState([]); // Use state for items
 
-  const content = items.map((item) => {
-    const attributesPath = "../../assets/attributes/";
-    const icon = new URL(
-      `${attributesPath}${item.icon.toLowerCase()}`,
-      import.meta.url
-    ).pathname;
-    // I'll take note na you
+    useEffect(() => {
+        const fetchIcons = async () => {
+            const iconPromises = originalItems.map(async (item) => {
+                try {
+                    const res = await fetchIcon(item.icon);
+                    return {
+                        ...item,
+                        icon: res,
+                    };
+                } catch (error) {
+                    console.log(error);
+                    return null;
+                }
+            });
+
+            const newItems = await Promise.all(iconPromises);
+            setItems(newItems); // Update items using state
+        };
+
+        fetchIcons();
+    }, [originalItems, fetchIcon]);
+
+    const handleClick = (id) => {
+        if (button === "checkbox") {
+            if (chips.includes(id)) {
+                setChips(chips.filter((chipId) => chipId !== id));
+                props.onChipValue(chips.filter((chipId) => chipId !== id));
+            } else {
+                setChips([...chips, id]);
+                props.onChipValue([...chips, id]);
+            }
+        } else {
+            setChips([id]);
+            props.onChipValue([id]);
+        }
+    };
+
+    const content = items.map((item) => {
+        return (
+            <Chip
+                key={item.id}
+                icon={
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: item.icon,
+                        }}
+                    />
+                }
+                label={item.name}
+                sx={{
+                    padding: "8px 18px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: "8px",
+                    justifyContent: "center",
+                    fontWeight: "500",
+                    fontSize: "16px",
+                    background: chips.includes(item.id)
+                        ? "rgba(3, 176, 119, 0.08)"
+                        : "default",
+                    color: chips.includes(item.id)
+                        ? "var(--accent)"
+                        : "inherit",
+                    "& svg": {
+                        fill: chips.includes(item.id)
+                            ? "var(--accent)"
+                            : "#8A93A6",
+                            height: '16px',
+                            width: '16px'
+                    },
+                    "& span": {
+                        padding: "0",
+                    },
+                    "& MuiChip-icon": {
+                        margin: "0",
+                    },
+                }}
+                onClick={clickable ? () => handleClick(item.id) : undefined}
+            />
+        );
+    });
 
     return (
-      <Chip
-        key={item.id}
-        icon={<img src={icon} alt={item.name} />}
-        label={item.name}
-        sx={{
-          padding: "18px",
-          fontWeight: "500",
-          fontSize: "16px",
-          background: chips.includes(item.id)
-            ? "rgba(3, 176, 119, 0.08)"
-            : "default",
-          color: chips.includes(item.id) ? "var(--accent)" : "inherit",
-          // "& svg": {
-          //   fill: chips.includes(item.id) ? "var(--accent)" : "#8A93A6",
-          // },
-        }}
-        onClick={clickable ? () => handleClick(item.id) : undefined}
-      />
+        <Stack direction="row" spacing={1}>
+            <div className={styles.chip}>{content}</div>
+        </Stack>
     );
-  });
-
-  return (
-    <Stack direction="row" spacing={1}>
-      <div className={styles.chip}>{content}</div>
-    </Stack>
-  );
 };
 
 export default ChipFilled;
