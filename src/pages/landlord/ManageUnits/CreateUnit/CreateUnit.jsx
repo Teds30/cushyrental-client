76cf6763dd92d850f0741856bd7a80bb6320b4f5
Ted30 <teddymarcenaje@gmail.com>
2023-10-26 01:36:39 +0800

    UI and minor bug fixes

--- src/pages/landlord/ManageUnits/CreateUnit/CreateUnit.jsx
+++ src/pages/landlord/ManageUnits/CreateUnit/CreateUnit.jsx
@@ -1,19 +1,49 @@
-import CreateUnitForm from "./CreateUnitForm";
+import CreateUnitForm from './CreateUnitForm'
 
-import styles from "./CreateUnit.module.css";
+import styles from './CreateUnit.module.css'
+import { Link, useNavigate } from 'react-router-dom'
+import { Box, IconButton } from '@mui/material'
+import { FiChevronLeft } from 'react-icons/fi'
 
 const CreateUnit = () => {
+    const navigate = useNavigate()
     return (
         <div className={styles.container}>
-            <div className={`${styles["container-title"]}`}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+            >
+                <Link
+                    to={`/manage_unit`}
+                    className={`${styles['link-button']}`}
+                >
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <FiChevronLeft
+                            style={{
+                                color: 'var(--fc-strong)',
+                                fill: 'transparent',
+                            }}
+                        />
+                    </IconButton>
+                    <span>Back</span>
+                </Link>
+            </Box>
+            <div className={`${styles['container-title']}`}>
                 <h1>Create Unit</h1>
             </div>
 
-            <div className={`${styles["main-container"]}`}>
+            <div className={`${styles['main-container']}`}>
                 <CreateUnitForm />
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default CreateUnit;
+export default CreateUnit
