10215c236e11f58c35c3ec6ab2582399630d8183
Ted30 <teddymarcenaje@gmail.com>
2023-10-26 01:29:03 +0800

    fixed switch value

--- src/pages/auth/CreateAccount/UserToggleButton.jsx
+++ src/pages/auth/CreateAccount/UserToggleButton.jsx
@@ -1,61 +1,65 @@
-import React, { useState } from "react";
-import ToggleButton from "@mui/material/ToggleButton";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import React, { useEffect, useState } from 'react'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 
 export default function UserToggleButton(props) {
-  const { onUserType } = props;
-  const [alignment, setAlignment] = useState("tenant");
+    const { onUserType } = props
+    const [alignment, setAlignment] = useState('tenant')
 
-  const handleChange = (event, newAlignment) => {
-    if (
-      newAlignment === null &&
-      (alignment === "tenant" || alignment === "landlord")
-    ) {
-      return;
+    const handleChange = (event, newAlignment) => {
+        if (
+            newAlignment === null &&
+            (alignment === 'tenant' || alignment === 'landlord')
+        ) {
+            return
+        }
+
+        setAlignment(newAlignment)
+        console.log(newAlignment)
     }
 
-    setAlignment(newAlignment);
-    onUserType({ user_type_id: newAlignment === "tenant" ? "1" : "2" });
-  };
+    useEffect(() => {
+        onUserType({ user_type_id: alignment === 'tenant' ? '3' : '2' })
+    }, [alignment])
 
-  return (
-    <ToggleButtonGroup
-      value={alignment}
-      exclusive
-      onChange={handleChange}
-      aria-label="Platform"
-      sx={{ fontSize: "18px", width: "100%" }}
-    >
-      <ToggleButton
-        value="tenant"
-        sx={{
-          "&.MuiToggleButton-root.Mui-selected": {
-            backgroundColor: "var(--accent)",
-            borderRadius: "4px",
-            boxShadow: "0px 2px 2px 0px rgba(0, 0, 0, 0.25)",
-            color: "var(--bg-layer1)",
-          },
-          width: "100%",
-          fontWeight: "600",
-        }}
-      >
-        Tenant
-      </ToggleButton>
-      <ToggleButton
-        value="landlord"
-        sx={{
-          "&.MuiToggleButton-root.Mui-selected": {
-            backgroundColor: "var(--accent)",
-            borderRadius: "4px",
-            boxShadow: "0px 2px 2px 0px rgba(0, 0, 0, 0.25)",
-            color: "var(--bg-layer1)",
-          },
-          width: "100%",
-          fontWeight: "600",
-        }}
-      >
-        Landlord
-      </ToggleButton>
-    </ToggleButtonGroup>
-  );
+    return (
+        <ToggleButtonGroup
+            value={alignment}
+            exclusive
+            onChange={handleChange}
+            aria-label="Platform"
+            sx={{ fontSize: '18px', width: '100%' }}
+        >
+            <ToggleButton
+                value="tenant"
+                sx={{
+                    '&.MuiToggleButton-root.Mui-selected': {
+                        backgroundColor: 'var(--accent)',
+                        borderRadius: '4px',
+                        boxShadow: '0px 2px 2px 0px rgba(0, 0, 0, 0.25)',
+                        color: 'var(--bg-layer1)',
+                    },
+                    width: '100%',
+                    fontWeight: '600',
+                }}
+            >
+                Tenant
+            </ToggleButton>
+            <ToggleButton
+                value="landlord"
+                sx={{
+                    '&.MuiToggleButton-root.Mui-selected': {
+                        backgroundColor: 'var(--accent)',
+                        borderRadius: '4px',
+                        boxShadow: '0px 2px 2px 0px rgba(0, 0, 0, 0.25)',
+                        color: 'var(--bg-layer1)',
+                    },
+                    width: '100%',
+                    fontWeight: '600',
+                }}
+            >
+                Landlord
+            </ToggleButton>
+        </ToggleButtonGroup>
+    )
 }
