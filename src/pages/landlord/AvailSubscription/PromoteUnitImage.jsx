9e19a7a6640871369b38dd9ed802f7b2e327cabf
Ted30 <teddymarcenaje@gmail.com>
2023-10-26 01:38:12 +0800

    added new component

---
+++ src/pages/landlord/AvailSubscription/PromoteUnitImage.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+import useImageManager from '../../../hooks/data/image-hook'
+const PromoteUnitImage = (props) => {
+    const { image } = props
+
+    const { fetchImage } = useImageManager()
+    const [imageBlob, setImageBlob] = useState()
+
+    useEffect(() => {
+        const loadData = async () => {
+            const res = await fetchImage(image)
+            setImageBlob(res)
+        }
+
+        loadData()
+    }, [])
+
+    return <img src={imageBlob} alt="" />
+}
+
+export default PromoteUnitImage
