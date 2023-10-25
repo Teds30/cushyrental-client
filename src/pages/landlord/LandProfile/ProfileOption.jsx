512d924d25dc52c84a63f5d68c45e44037ed95f1
Ted30 <teddymarcenaje@gmail.com>
2023-10-26 01:11:49 +0800

    added user condition

--- src/pages/landlord/LandProfile/ProfileOption.jsx
+++ src/pages/landlord/LandProfile/ProfileOption.jsx
@@ -1,29 +1,52 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-import styles from './LandLordProfile.module.css';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
-import { BiBuildingHouse } from "react-icons/bi";
+import styles from './LandLordProfile.module.css'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions'
+import { BiBuildingHouse } from 'react-icons/bi'
 
 const ProfileOption = (props) => {
+    const { user_type_id } = props
     return (
         <div {...props} className={`${styles['profile-container']}`}>
-            <Link to='/profile/user_profile/1' className={styles.col}>
-                <PersonOutlineIcon style={{color: 'var(--fc-strong)', height: '30px', width: '30px'}} />
-                <p className='smaller-text'>My Profile</p>
+            <Link to="/profile/user_profile" className={styles.col}>
+                <PersonOutlineIcon
+                    style={{
+                        color: 'var(--fc-strong)',
+                        height: '30px',
+                        width: '30px',
+                    }}
+                />
+                <p className="smaller-text">My Profile</p>
             </Link>
 
-            <Link className={styles.col}>
-                <SubscriptionsIcon style={{color: 'var(--fc-strong)', height: '30px', width: '30px'}} />
-                <p className='smaller-text'>Subscription</p>
-            </Link>
+            {user_type_id === 2 && (
+                <Link to="/manage_subscriptions" className={styles.col}>
+                    <SubscriptionsIcon
+                        style={{
+                            color: 'var(--fc-strong)',
+                            height: '30px',
+                            width: '30px',
+                        }}
+                    />
+                    <p className="smaller-text">Subscription</p>
+                </Link>
+            )}
 
-            <Link to='/manage_unit' className={styles.col}>
-                <BiBuildingHouse style={{color: 'var(--fc-strong)', height: '30px', width: '30px'}} />
-                <p className='smaller-text'>My Unit</p>
-            </Link>
+            {user_type_id === 2 && (
+                <Link to="/manage_unit" className={styles.col}>
+                    <BiBuildingHouse
+                        style={{
+                            color: 'var(--fc-strong)',
+                            height: '30px',
+                            width: '30px',
+                        }}
+                    />
+                    <p className="smaller-text">My Unit</p>
+                </Link>
+            )}
         </div>
-    );
-};
+    )
+}
 
-export default ProfileOption;
+export default ProfileOption
