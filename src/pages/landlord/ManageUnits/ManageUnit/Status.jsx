33065fb6d495966cfde5f801878a8bb1e03aac3c
Ted30 <teddymarcenaje@gmail.com>
2023-10-26 01:39:17 +0800

    minor changes

--- src/pages/landlord/ManageUnits/ManageUnit/Status.jsx
+++ src/pages/landlord/ManageUnits/ManageUnit/Status.jsx
@@ -1,25 +1,25 @@
-import StatusChip from "../../../../components/StatusChip/StatusChip";
+import StatusChip from '../../../../components/StatusChip/StatusChip'
 
 const Status = (props) => {
-  const { unitRequestStatus } = props;
+    const { unitRequestStatus } = props
 
-  let status;
+    let status
 
-  if (unitRequestStatus === 0) {
-    status = { type: "pending", status: "Pending" };
-  } else if (unitRequestStatus === 1) {
-    status = { type: "success", status: "Listed" };
-  } else if (unitRequestStatus === 2) {
-    status = { type: 'cancel', status: "Unlisted" };
-  } else {
-    status = { type: "danger", status: "Rejected" };
-  }
+    if (unitRequestStatus === 0) {
+        status = { type: 'pending', status: 'PENDING' }
+    } else if (unitRequestStatus === 1) {
+        status = { type: 'success', status: 'LISTED' }
+    } else if (unitRequestStatus === 2) {
+        status = { type: 'cancel', status: 'UNLISTED' }
+    } else {
+        status = { type: 'danger', status: 'REJECTED' }
+    }
 
-  return (
-    <StatusChip type={status.type}>
-      {status.status[0].toUpperCase() + status.status.slice(1)}
-    </StatusChip>
-  );
-};
+    return (
+        <StatusChip type={status.type}>
+            {status.status[0].toUpperCase() + status.status.slice(1)}
+        </StatusChip>
+    )
+}
 
-export default Status;
+export default Status
