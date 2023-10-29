991a56f8a8a585420c4f3678b6ed17b8fc664e8c
Ted30 <teddymarcenaje@gmail.com>
2023-10-29 14:02:43 +0800

    DP-37 | added map & updated after search

---
+++ src/pages/tenant/UnitAfterSearch/InfoWindowUnit.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import PrimaryButton from '../../../components/Button/PrimaryButton'
+
+import styles from './InfoWindowUnit.module.css'
+
+import useImageManager from '../../../hooks/data/image-hook'
+import { useNavigate } from 'react-router-dom'
+
+const InfoWindowUnit = (props) => {
+    const { unit } = props
+    const navigate = useNavigate()
+
+    const { fetchImage, isLoading } = useImageManager()
+    const [unitImg, setUnitImg] = useState()
+
+    useEffect(() => {
+        const handleFetch = async () => {
+            console.log(unit)
+            try {
+                const res = await fetchImage(unit.images[0].image)
+                setUnitImg(res)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        handleFetch()
+    }, [unit])
+    return (
+        <div className={styles['info-unit-container']}>
+            <div className={styles['info-unit-image']}>
+                <img src={unitImg} alt="" />
+            </div>
+            <div>
+                <h2>₱{unit.price}</h2>
+                <p>{unit.name}</p>
+            </div>
+
+            <PrimaryButton
+                width="100%"
+                onClick={() => {
+                    navigate(`/unit/${unit.id}`)
+                }}
+            >
+                View Unit
+            </PrimaryButton>
+        </div>
+    )
+}
+
+export default InfoWindowUnit
--- src/pages/tenant/UnitAfterSearch/MapAfterSearch.jsx
+++ src/pages/tenant/UnitAfterSearch/MapAfterSearch.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    Circle,
+    GoogleMap,
+    InfoWindow,
+    Marker,
+    useLoadScript,
+} from '@react-google-maps/api'
 
-const MapAfterSearch = () => {
-  return (
-    <div> MAPA MAPA MAPA MAPA</div>
-  )
+import styles from './UnitAfterSearch.module.css'
+import PrimaryButton from '../../../components/Button/PrimaryButton'
+import InfoWindowUnit from './InfoWindowUnit'
+
+const MapAfterSearch = (props) => {
+    const { center, radius, units } = props
+    const [mapref, setMapRef] = useState()
+    const [activeMarker, SetActiveMarker] = useState()
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API,
+    })
+
+    const handleOnLoad = (map) => {
+        setMapRef(map)
+    }
+
+    const option = {
+        strokeColor: '#2666b7',
+        strokeOpacity: 0.4,
+        strokeWeight: 2,
+        fillColor: '#2666b7',
+        fillOpacity: 0.15,
+        clickable: false,
+        draggable: false,
+        editable: false,
+        visible: true,
+        radius:
+            radius === 1
+                ? 500
+                : radius === 2
+                ? 1000
+                : radius === 3
+                ? 2000
+                : 500,
+        zIndex: 1,
+    }
+
+    const changeActiveMarker = (id) => {
+        SetActiveMarker(id)
+    }
+
+    let unitMarkers = <></>
+    unitMarkers = units.map((unit) => {
+        const [lat, lng] = unit.location.split(', ').map(Number)
+        const coords = { lat, lng }
+        return (
+            <Marker
+                key={unit.id}
+                icon={{ url: '/assets/unit.svg' }}
+                position={coords}
+                onClick={() => {
+                    changeActiveMarker(unit.id)
+                }}
+            >
+                {activeMarker === unit.id ? (
+                    <InfoWindow
+                        onCloseClick={() => SetActiveMarker(null)}
+                        className={styles['info-window']}
+                    >
+                        <InfoWindowUnit unit={unit} />
+                    </InfoWindow>
+                ) : null}
+            </Marker>
+        )
+    })
+
+    return (
+        <div className={styles['padding-top-container']}>
+            <div className={styles['map-container']}>
+                {isLoaded && (
+                    <GoogleMap
+                        id="map"
+                        zoom={17}
+                        options={{
+                            mapId: 'd9e80ee42fd50abc',
+                            fullscreenControl: true,
+                            zoomControl: false,
+                            mapTypeControl: true,
+                            streetViewControl: true,
+                        }}
+                        center={center}
+                        mapContainerClassName={styles['map-container']}
+                        onLoad={handleOnLoad}
+                    >
+                        <Marker position={center}></Marker>
+                        {unitMarkers}
+                        <Circle center={center} options={option}></Circle>
+                    </GoogleMap>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default MapAfterSearch
