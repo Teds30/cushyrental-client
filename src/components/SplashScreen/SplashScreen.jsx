71f2ac1557c5962c2636bb1d6794fae9026a3963
Ted30 <teddymarcenaje@gmail.com>
2023-11-17 16:52:28 +0800

    added splashscreen

---
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './SplashScreen.module.css'
+
+import brand_logo from '../../assets/cushyrental.svg'
+
+const SplashScreen = () => {
+    return <div className={styles['container']}>
+    
+    </div>
+}
+
+export default SplashScreen
