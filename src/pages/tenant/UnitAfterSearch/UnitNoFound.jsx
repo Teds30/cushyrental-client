91c65472a60d597b4391964425ec8bd3bb169fb4
Ted30 <teddymarcenaje@gmail.com>
2023-11-25 04:31:45 +0800

    added back button link

--- src/pages/tenant/UnitAfterSearch/UnitNoFound.jsx
+++ src/pages/tenant/UnitAfterSearch/UnitNoFound.jsx
@@ -1,25 +1,34 @@
-import React from "react";
-import styles from "./UnitNoFound.module.css";
-import PrimaryButton from "../../../components/Button/PrimaryButton";
-import gif from "../../../assets/searching_the_web.gif";
+import React from 'react'
+import styles from './UnitNoFound.module.css'
+import PrimaryButton from '../../../components/Button/PrimaryButton'
+import gif from '../../../assets/searching_the_web.gif'
+import { useNavigate } from 'react-router-dom'
 
 const UnitNoFound = () => {
+    const navigate = useNavigate()
+
     return (
-        <div className={`${styles["nofound-container"]} `}>
-            <div className={`${styles["gif-container"]} `}>
+        <div className={`${styles['nofound-container']} `}>
+            <div className={`${styles['gif-container']} `}>
                 <img src={gif} alt="gif" />
             </div>
-            <div className={`${styles["nosearchfound-text"]} `}>
+            <div className={`${styles['nosearchfound-text']} `}>
                 <p>No Search Found</p>
             </div>
-            <div className={`${styles["lookingfor-text"]} `}>
+            <div className={`${styles['lookingfor-text']} `}>
                 <p>We couldn't find what your looking for</p>
             </div>
-            <div className={`${styles["button-container"]} `}>
-                <PrimaryButton>Search Again</PrimaryButton>
+            <div className={`${styles['button-container']} `}>
+                <PrimaryButton
+                    onClick={() => {
+                        navigate('/search')
+                    }}
+                >
+                    Search Again
+                </PrimaryButton>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default UnitNoFound;
+export default UnitNoFound
