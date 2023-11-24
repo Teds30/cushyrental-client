8daa98f2dc98f813edbb4b91f06a70e264215b8a
Ted30 <teddymarcenaje@gmail.com>
2023-11-25 04:15:48 +0800

    changed icons

--- src/pages/tenant/SearchUnit/Location.jsx
+++ src/pages/tenant/SearchUnit/Location.jsx
@@ -32,18 +32,46 @@ const locations = [
         id: 1,
         name: 'Location',
         fixedIcon: (
-            <TbMapPin
-                style={{
-                    fill: 'transparent',
-                    color: 'inherit',
-                }}
-            />
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-location"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                color="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
+            </svg>
         ),
     },
     {
         id: 2,
         name: 'Institution',
-        fixedIcon: <FaUniversity />,
+        fixedIcon: (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-school"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                color="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
+                <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+            </svg>
+        ),
     },
 ]
 
@@ -172,7 +200,7 @@ const Location = (props) => {
                                 onPlacesChanged={onPlacesChanged}
                                 onLoad={onSBLoad}
                             >
-                                <SearchField placeholder="Search" />
+                                <SearchField placeholder="example: 'Rawis' or 'Daraga'" />
                             </StandaloneSearchBox>
                         )}
                         {searchType === 2 && (
--- src/pages/tenant/SearchUnit/LocationDropdown.jsx
+++ src/pages/tenant/SearchUnit/LocationDropdown.jsx
@@ -26,15 +26,32 @@ export default function LocationDropdown(props) {
     return (
         <Box sx={{ minWidth: 120, position: 'relative' }}>
             <FormControl fullWidth>
-                <IoLocationOutline
-                    size={25}
+                <span
                     style={{
                         position: 'absolute',
-                        bottom: '16px',
+                        bottom: '10px',
                         left: '10px',
-                        fill: '#5C6173',
+                        fill: 'none',
                     }}
-                />
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-school"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        fill="none"
+                        color="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
+                        <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+                    </svg>
+                </span>
                 <Select
                     // labelId="demo-simple-select-label"
                     // id="demo-simple-select"
