be1c0c752881ebc6609209e0669cf4f531cf2753
Ted30 <teddymarcenaje@gmail.com>
2023-11-30 23:55:52 +0800

    UI changes

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -35,11 +35,10 @@ const About = () => {
                     <li>Home</li>
                     <li>Home</li>
                 </ul>
-                <div className={styles['cta']}>
-                    <Link className={styles['primary-btn']} to={'/'}>
-                        Board Now
-                    </Link>
-                </div>
+
+                <Link className={styles['primary-btn']} to={'/signin'}>
+                    Board Now
+                </Link>
             </div>
             <section className={styles['intro']}>
                 <div className={styles['blur-img']}>
@@ -69,7 +68,7 @@ const About = () => {
             <section className={styles['misvis_container']}>
                 <span
                     className={styles['wave']}
-                    style={{ marginBottom: '-6px' }}
+                    style={{ marginBottom: '-8px' }}
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -480,11 +479,56 @@ const About = () => {
                 <div className={styles['contact']}>
                     <div className={styles['contact-item']}>
                         <p className="title">Socials</p>
-                        <p>O</p>
+                        <div className={styles['socials']}>
+                            <div className={styles['social-brand']}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="icon icon-tabler icon-tabler-brand-facebook"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="2"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                                </svg>
+                            </div>
+                            <div className={styles['social-brand']}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="icon icon-tabler icon-tabler-brand-instagram"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="2"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                >
+                                    <path
+                                        stroke="none"
+                                        d="M0 0h24v24H0z"
+                                        fill="none"
+                                    />
+                                    <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+                                    <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                                    <path d="M16.5 7.5l0 .01" />
+                                </svg>
+                            </div>
+                        </div>
                     </div>
                     <div className={styles['contact-item']}>
                         <p className="title">Email Address</p>
-                        <p>cushyrental@gmail.com</p>
+                        <p>developers@cushyrental.com</p>
                     </div>
                     <div className={styles['contact-item']}>
                         <p className="title">Phone Number</p>
