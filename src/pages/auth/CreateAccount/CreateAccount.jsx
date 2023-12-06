ab9513be7a50fd9cd00a2de151bc3acd1b1e77e3
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2023-12-06 14:03:26 +0800

    Facebook | update

--- src/pages/auth/CreateAccount/CreateAccount.jsx
+++ src/pages/auth/CreateAccount/CreateAccount.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useContext } from 'react'
+import { useState, useCallback, useContext, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 import UserToggleButton from './UserToggleButton'
@@ -120,59 +120,51 @@ const CreateAccount = () => {
         // } catch (error) {}
     // }
 
-    window.fbAsyncInit = function () {
-        // FB JavaScript SDK configuration and setup
-        FB.init({
-            appId: "863373298592556", // FB App ID
-            cookie: true, // enable cookies to allow the server to access the session
-            xfbml: true, // parse social plugins on this page
-            version: "v3.2", // use graph api version 2.8
-        });
-
-        // Check whether the user already logged in
-        FB.getLoginStatus(function (response) {
-            if (response.status === "connected") {
-                //display user data
-                getFbUserData();
-            }
-        });
-    };
-
-    // Load the JavaScript SDK asynchronously
-    (function (d, s, id) {
-        var js,
-            fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-
-    // Facebook login with JavaScript SDK
-    function facebookRegisterHandler() {
-        FB.login(
-            function (response) {
-                if (response.authResponse) {
-                    // Get and display the user profile data
-                    getFbUserData();
-                    // document.getElementById('status').innerHTML = 'User cancelled login or did not fully authorize.';
-                }
-            },
-            { scope: "email" }
-        );
-    }
-
-    const SubmitFaceBookData = async (data) => {
+    const facebookRegisterHandler = async () => {
         try {
-                const res = await facebookAccountRegistration(data)
-                console.log(res);
-                ctx.onLogin({ user: res.user, token: res.token })
-                navigate('/');
-            } catch (error) {
-                notify('Email already exist.', 'info')
-            }
-    }
+          // Assuming the Facebook SDK is already loaded
+          const response = await new Promise((resolve) =>
+            window.FB.login(resolve, { scope: 'email' })
+          );
+    
+          if (response.authResponse) {
+            getFbUserData();
+          }
+        } catch (error) {
+          console.error('Error during Facebook login:', error);
+        }
+      };
+    
+      useEffect(() => {
+        // Load the Facebook SDK asynchronously
+        const loadFacebookSDK = () => {
+          window.fbAsyncInit = function () {
+            window.FB.init({
+              appId: '863373298592556', // Replace with your FB App ID
+              cookie: true,
+              xfbml: true,
+              version: 'v12.0', // Use the latest version
+            });
+    
+            window.FB.getLoginStatus(function (response) {
+              if (response.status === 'connected') {
+                getFbUserData();
+              }
+            });
+          };
+    
+          (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = '//connect.facebook.net/en_US/sdk.js';
+            fjs.parentNode.insertBefore(js, fjs);
+          })(document, 'script', 'facebook-jssdk');
+        };
+    
+        loadFacebookSDK();
+      }, []); // Empty dependency array ensures this effect runs only once on component mount
 
     const getFbUserData = () => {
         window.FB.api(
