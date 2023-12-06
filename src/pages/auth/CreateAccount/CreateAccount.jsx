e74115747c2d61c98009f05228c99e18bc925bcd
JohnOtilla09 <johncanila.otilla@bicol-u.edu.ph>
2023-12-06 14:58:17 +0800

    Facebook | update

--- src/pages/auth/CreateAccount/CreateAccount.jsx
+++ src/pages/auth/CreateAccount/CreateAccount.jsx
@@ -1,61 +1,61 @@
-import { useState, useCallback, useContext, useEffect } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useState, useCallback, useContext, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-import UserToggleButton from './UserToggleButton'
-import CreateAccountForm from './CreateAccountForm'
-import { useGoogleLogin } from '@react-oauth/google'
-import useAuth from '../../../hooks/data/auth-hook'
-import useGoogleAuth from '../../../hooks/data/google-auth-hook'
-import useFacebookAuth from '../../../hooks/data/facebook-auth'
-import AuthContext from '../../../context/auth-context'
-import useNotistack from '../../../hooks/notistack-hook'
+import UserToggleButton from "./UserToggleButton";
+import CreateAccountForm from "./CreateAccountForm";
+import { useGoogleLogin } from "@react-oauth/google";
+import useAuth from "../../../hooks/data/auth-hook";
+import useGoogleAuth from "../../../hooks/data/google-auth-hook";
+import useFacebookAuth from "../../../hooks/data/facebook-auth";
+import AuthContext from "../../../context/auth-context";
+import useNotistack from "../../../hooks/notistack-hook";
 
-import styles from './CreateAccount.module.css'
+import styles from "./CreateAccount.module.css";
 
 const CreateAccount = () => {
-    const { accountRegistration, isLoading } = useAuth()
-    const { googleAccountRegistration, googleAuth } = useGoogleAuth()
-    const { facebookAccountRegistration } = useFacebookAuth()
-    const ctx = useContext(AuthContext)
-    const navigate = useNavigate()
-    const { notify } = useNotistack()
-    const receivedData = useLocation()
+    const { accountRegistration, isLoading } = useAuth();
+    const { googleAccountRegistration, googleAuth } = useGoogleAuth();
+    const { facebookAccountRegistration } = useFacebookAuth();
+    const ctx = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { notify } = useNotistack();
+    const receivedData = useLocation();
 
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
     const [userType, setUserType] = useState({
         user_type_id:
             receivedData.state && receivedData.state.user_type_id
                 ? receivedData.state.user_type_id
                 : 2,
-    })
+    });
 
     const userTypeHandler = useCallback(
         (userType) => {
-            setUserType(userType)
+            setUserType(userType);
         },
         [setUserType]
-    )
+    );
 
     const createAccountHandler = async (userData) => {
         const data = {
             ...userData,
             user_type_id: userType.user_type_id,
-        }
+        };
 
         try {
-            const res = await accountRegistration(data)
+            const res = await accountRegistration(data);
 
-            ctx.onLogin({ user: res.user, token: res.token })
+            ctx.onLogin({ user: res.user, token: res.token });
             // navigate('/')
         } catch (error) {
-            notify('Email already exist.', 'info')
+            notify("Email already exist.", "info");
         }
-    }
+    };
 
     const googleRegisterHandle = useGoogleLogin({
         onSuccess: async (credentialResponse) => {
             try {
-                const res = await googleAuth(credentialResponse)
+                const res = await googleAuth(credentialResponse);
 
                 const data = {
                     first_name: res.given_name,
@@ -63,105 +63,111 @@ const CreateAccount = () => {
                     email: res.email,
                     profile_picture_img: res.picture,
                     user_type_id: userType.user_type_id,
-                }
+                };
 
-                const registerGoogleRes = await googleAccountRegistration(data)
+                const registerGoogleRes = await googleAccountRegistration(data);
                 ctx.onLogin({
                     user: registerGoogleRes.user,
                     token: registerGoogleRes.token,
-                })
-                navigate('/')
+                });
+                navigate("/");
             } catch (error) {
-                notify('Email already exist.', 'info')
-            } {}
+                notify("Email already exist.", "info");
+            }
+            {
+            }
         },
-    })
+    });
 
     const facebookRegisterHandler = async () => {
         try {
-          // Assuming the Facebook SDK is already loaded
-          const response = await new Promise((resolve) =>
-            window.FB.login(resolve, { scope: 'email' })
-          );
-    
-          if (response.authResponse) {
-            getFbUserData();
-          }
-        } catch (error) {
-          console.error('Error during Facebook login:', error);
-        }
-      };
+            // Assuming the Facebook SDK is already loaded
+            const response = await new Promise((resolve) =>
+                window.FB.login(resolve, { scope: "email" })
+            );
 
-      // Load the Facebook SDK asynchronously
-      const loadFacebookSDK = () => {
-        window.fbAsyncInit = function () {
-          window.FB.init({
-            appId: '863373298592556', // Replace with your FB App ID
-            cookie: true,
-            xfbml: true,
-            version: 'v12.0', // Use the latest version
-          });
-  
-          window.FB.getLoginStatus(function (response) {
-            if (response.status === 'connected') {
-              getFbUserData();
+            if (response.authResponse) {
+                getFbUserData();
             }
-          });
+        } catch (error) {
+            console.error("Error during Facebook login:", error);
+        }
+    };
+
+    // Load the Facebook SDK asynchronously
+    const loadFacebookSDK = () => {
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                appId: "863373298592556", // Replace with your FB App ID
+                cookie: true,
+                xfbml: true,
+                version: "v12.0", // Use the latest version
+            });
+
+            window.FB.getLoginStatus(function (response) {
+                if (response.status === "connected") {
+                    getFbUserData();
+                }
+            });
         };
-  
+
         (function (d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s);
-          js.id = id;
-          js.src = '//connect.facebook.net/en_US/sdk.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        })(document, 'script', 'facebook-jssdk');
-      };
-    
-      useEffect(() => {
+            var js,
+                fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, "script", "facebook-jssdk");
+    };
+
+    useEffect(() => {
         loadFacebookSDK();
-      }, []);
+    }, []);
 
     const getFbUserData = () => {
         window.FB.api(
             "/me",
             { fields: "id,first_name,last_name,email,picture.width(200)" },
-           async function (response) {
+            async function (response) {
                 console.log(response);
 
                 const data = {
                     email: response.email,
                     first_name: response.first_name,
                     last_name: response.last_name,
+                    middle_name: 'middle_name',
                     profile_picture_img: response.picture.data.url,
                     user_type_id: userType.user_type_id,
-                }
+                };
 
                 try {
-                    const res = await facebookAccountRegistration(data)
+                    const res = await facebookAccountRegistration(data);
                     console.log(res);
-                    ctx.onLogin({ user: res.user, token: res.token })
-                    navigate('/');
-                } catch (error) {}
+                    ctx.onLogin({ user: res.user, token: res.token });
+                    navigate("/");
+                } catch (error) {
+                    notify("Email already exist.", "info");
+                }
             }
         );
     };
 
     return (
         <div className={styles.container}>
-            <div className={`${styles['container-title']}`}>
+            <div className={`${styles["container-title"]}`}>
                 <h2>Create Account</h2>
             </div>
-            <div className={`${styles['main-container']}`}>
-                <div className={`${styles['main-container-type']}`}>
+            <div className={`${styles["main-container"]}`}>
+                <div className={`${styles["main-container-type"]}`}>
                     <UserToggleButton
                         onUserType={userTypeHandler}
                         initialSelected={userType}
                     />
                 </div>
 
-                <div className={`${styles['main-container-form']}`}>
+                <div className={`${styles["main-container-form"]}`}>
                     <CreateAccountForm
                         onCreateAccount={createAccountHandler}
                         isLoading={isLoading}
@@ -171,7 +177,7 @@ const CreateAccount = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CreateAccount
+export default CreateAccount;
